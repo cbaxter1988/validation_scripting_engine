dbfa169995b6863a22dd84e999b925040c94bed5
byt3-m3 <cbaxtertech@gmail.com>
2020-06-30 02:16:15 +0300

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='vse',
+   version='1.0',
+   description='Validation Scripting Engine',
+   author='Courtney S Baxter Jr',
+   author_email='cbaxtertech@gmail.com',
+   packages=find_packages(),
+
+)
+
